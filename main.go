004d0ad3da385d87e57aee02148a1f121b29461b
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-03-07 14:56:58 +0100

    Open db

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	bolt "go.etcd.io/bbolt"
+)
 
 func main() {
+	db, err := bolt.Open("books.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	fmt.Println("Hello bbolt!")
 }
-
